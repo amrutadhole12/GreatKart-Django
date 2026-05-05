e714a5b30a6bb1079dbc266cb2ca971c25456393
amrutadhole12 <amrutadhole80@gmail.com>
2026-05-05 20:49:28 +0530

    django authentication

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -134,3 +134,16 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_HOST_USER = 'amrutadhole600@gmail.com'
+EMAIL_HOST_PASSWORD = 'Dhole@098'
+EMAIL_USE_TLS = True
+
